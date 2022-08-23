97ce7a9cb8bb0224cc77f7daf14bfc141e44a605
salazbr <salazbr@oocl.com>
2022-08-23 14:14:53 +0800

    Add answers in index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+// Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
+var name = 'hello';
+
+
+// TODO write your code here...
+name.toUpperCase(); 
+
+
+// Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
+var sentence = 'good afternoon, mr mike.';
+//TODO write your code here...
+sentence.toLocaleUpperCase;
+console.log(sentence.toUpperCase());
+
+// Complete the following procedure to extract the amount of money variable, output: 20.
+var money = '￥20';
+//TODO write your code here...
+money.slice(1);
+
